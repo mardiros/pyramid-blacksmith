79a1ada737973792a444a91dde93a3992fea1f2d
Guillaume Gauvrit <guillaume@gauvr.it>
2022-01-06 20:58:05 +0100

    Add a way to build sd implementation

--- src/pyramid_blacksmith/binding.py
+++ src/pyramid_blacksmith/binding.py
@@ -1,12 +1,66 @@
-from blacksmith import SyncClientFactory, SyncConsulDiscovery
+from typing import Callable, Dict, Protocol
+from blacksmith import (
+    SyncClientFactory,
+    SyncConsulDiscovery,
+    SyncStaticDiscovery,
+    SyncRouterDiscovery,
+)
+from blacksmith.sd._sync.base import SyncAbstractServiceDiscovery
 
 from pyramid.config import Configurator
+from pyramid.request import Request
+from pyramid.exceptions import ConfigurationError
 
 
-def blacksmith_binding_factory(config):
-    def blacksmith_binding(request):
+SD_KEY = "blacksmith.service_discovery"
+
+
+class SDBuilder(Protocol):
+    def __call__(self, settings: Dict[str, str]) -> SyncAbstractServiceDiscovery:
+        ...
+
+
+def build_sd_static(settings: Dict[str, str]) -> SyncStaticDiscovery:
+    pass
+
+
+def build_sd_consul(settings: Dict[str, str]) -> SyncConsulDiscovery:
+    pass
+
+
+def build_sd_router(settings: Dict[str, str]) -> SyncRouterDiscovery:
+    pass
+
+
+def get_sd_strategy(settings: Dict[str, str]) -> SDBuilder:
+    sd_classes: Dict[str, SDBuilder] = {
+        "static": build_sd_static,
+        "consul": build_sd_consul,
+        "router": build_sd_router,
+    }
+    sd_name = settings.get(SD_KEY)
+    if not sd_name:
+        raise ConfigurationError(f"Missing setting {SD_KEY}")
+
+    if sd_name not in sd_classes:
+        raise ConfigurationError(
+            f"Invalid value {sd_name} for {SD_KEY}: "
+            f"not in {', '.join(sd_classes.keys())}"
+        )
+
+    return sd_classes[sd_name]
+
+
+def blacksmith_binding_factory(
+    config: Configurator,
+) -> Callable[[Request], SyncClientFactory]:
+
+    settings = config.registry.settings
+
+    def blacksmith_binding(request: Request) -> SyncClientFactory:
         sd = SyncConsulDiscovery()
         return SyncClientFactory(sd)
+
     return blacksmith_binding
 
 
--- tests/unittests/test_includeme.py
+++ tests/unittests/test_includeme.py
@@ -1,5 +1,14 @@
 from blacksmith.service._sync.client import SyncClientFactory
+from pyramid.exceptions import ConfigurationError
 from pyramid.interfaces import IRequestExtensions
+import pytest
+
+from pyramid_blacksmith.binding import (
+    build_sd_consul,
+    build_sd_router,
+    build_sd_static,
+    get_sd_strategy,
+)
 
 
 def test_includeme(config):
@@ -9,3 +18,46 @@ def test_includeme(config):
 
 def test_req_attr(dummy_request):
     assert isinstance(dummy_request.blacksmith, SyncClientFactory)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        {
+            "settings": {"blacksmith.service_discovery": "static"},
+            "expected": build_sd_static,
+        },
+        {
+            "settings": {"blacksmith.service_discovery": "consul"},
+            "expected": build_sd_consul,
+        },
+        {
+            "settings": {"blacksmith.service_discovery": "router"},
+            "expected": build_sd_router,
+        },
+    ],
+)
+def test_get_sd_strategy(params):
+    assert get_sd_strategy(params["settings"]) == params["expected"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        {
+            "settings": {},
+            "expected": "Missing setting blacksmith.service_discovery",
+        },
+        {
+            "settings": {"blacksmith.service_discovery": "Static"},
+            "expected": (
+                "Invalid value Static for blacksmith.service_discovery: "
+                "not in static, consul, router"
+            ),
+        },
+    ],
+)
+def test_get_sd_strategy_error(params):
+    with pytest.raises(ConfigurationError) as ctx:
+        get_sd_strategy(params["settings"])
+    assert str(ctx.value) == params["expected"]
