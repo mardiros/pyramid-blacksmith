3aee24192df6b340c8db16f09ffa6650c802a43f
Guillaume Gauvrit <guillaume@gauvr.it>
2024-10-10 17:31:24 +0200

    Fix release number

--- src/pyramid_blacksmith/__init__.py
+++ src/pyramid_blacksmith/__init__.py
@@ -4,7 +4,7 @@ from .binding import PyramidBlacksmith, includeme
 from .middleware import AbstractMiddlewareBuilder
 from .middleware_factory import AbstractMiddlewareFactoryBuilder
 
-__version__ = metadata.version("blacksmith")
+__version__ = metadata.version("pyramid_blacksmith")
 
 __all__ = [
     "PyramidBlacksmith",
